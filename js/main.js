eee6751f8bfd9da54beedc90bd2859746abc9c3f
sameerpatil20144 <sameer.patil20144@gmail.com>
2018-04-19 05:45:42 +0530

    UPDATE

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Modal Box Start
+function display_modal_box(){
+    document.querySelector("#Mybox").style.display="block";    
+    document.querySelector("#modal").style.display="block";
+}
+function cancel_modal_box(){
+    document.querySelector("#Mybox").style.display="none";    
+    document.querySelector("#modal").style.display="none";
+}
+// Modal Box End
